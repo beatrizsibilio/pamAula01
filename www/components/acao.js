aa719c247c219c63cc6bd6a34f7d389e2e4a31ff
beatrizsibilio <beatrizduartesibilio@gmail.com>
2021-03-14 08:38:04 +0900

    calculadora completa

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,43 @@
 // This is a JavaScript file
+$(document).on('click','#subtrair', function(){
+  var valor1 = $('#valor1').val();
+  var valor2 = $('#valor2').val();
 
+  var resultado = parseFloat(valor1) - parseFloat(valor2);
+
+  $('#resultado').val(resultado);
+});
+
+$(document).on('click','#multiplicar', function(){
+  var valor1 = $('#valor1').val();
+  var valor2 = $('#valor2').val();
+
+  var resultado = parseFloat(valor1) * parseFloat(valor2);
+
+  $('#resultado').val(resultado);
+});
+
+$(document).on('click','#dividir', function(){
+  var valor1 = $('#valor1').val();
+  var valor2 = $('#valor2').val();
+
+  var resultado = parseFloat(valor1) / parseFloat(valor2);
+
+  $('#resultado').val(resultado);
+});
+
+$(document).on('click','#exponenciacao', function(){
+  var valor1 = $('#valor1').val();
+  var valor2 = $('#valor2').val();
+
+  var resultado = Math.pow(valor1,valor2);
+
+  $('#resultado').val(resultado);
+});
+
+/*$(document).ready(function() {
+  $("#subtrair").click(function(event) {
+    console.log("click")
+    var valor1 = $('#valor1').val();
+    var valor2 = $('#valor2').val();
+*/
